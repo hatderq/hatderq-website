9c25afd005a5b9f7ef77efba2121c554ef955190
hatderq <derqaouihatim@gmail.com>
2024-10-08 00:13:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Add event listener to the form submit button
-document.querySelector('.contact-us form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Get the form data
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const message = document.querySelector('#message').value;
-    // Send the form data to the server
-    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
-});
+let menu = document.querySelector('#menu-icon')
+let navlist = document.querySelector('.navelist')
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
